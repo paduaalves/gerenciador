57caa223d74e34c295aea98a30099c62f8b95669
Pádua Alves <paduaalves03@gmail.com>
2021-12-01 16:04:17 -0300

    Restringindo o acesso a todas as páginas

--- src/main/java/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
+++ src/main/java/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
@@ -8,6 +8,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.websocket.Session;
 
 import br.com.alura.gerenciador.acao.Acao;
 import br.com.alura.gerenciador.enums.TipoReposta;
@@ -21,6 +23,16 @@ public class UnicaEntradaServlet extends HttpServlet {
 	private static String ROOT_PATH="WEB-INF/view/";
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		try {
+			String paramAcao = request.getParameter("acao");
+			HttpSession sessao = request.getSession();
+			
+			boolean usuarioNaoEstaLogado = (sessao.getAttribute("usuario")==null);
+			boolean ehUmaAcaoProtegida = !(paramAcao.equals("Login") || paramAcao.equals("LoginForm"));
+			
+			if(ehUmaAcaoProtegida && usuarioNaoEstaLogado) {
+				response.sendRedirect("entrada?acao=LoginForm");
+				return;
+			}
 			Acao acao = (Acao) Class.forName("br.com.alura.gerenciador.acao." + request.getParameter("acao")).newInstance();
 			String destino = acao.executa(request, response);
 			if(TipoReposta.FOWARD.equals(acao.getTipoReposta())){
