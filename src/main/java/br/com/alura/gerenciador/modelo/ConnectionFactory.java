ac8749e93391be775d4d3de37b9bc6a930b43b62
Pádua Alves <paduaalves03@gmail.com>
2021-12-08 11:42:43 -0300

    Adicionando o pool de conexões

--- src/main/java/br/com/alura/gerenciador/modelo/ConnectionFactory.java
+++ src/main/java/br/com/alura/gerenciador/modelo/ConnectionFactory.java
@@ -1,32 +1,46 @@
 package br.com.alura.gerenciador.modelo;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 public class ConnectionFactory {
 
-    private static Connection connection;
+    private static DataSource dataSource;
 
-    static{
+    static {
         new ConnectionFactory();
     }
 
-    public  ConnectionFactory(){
+    private ConnectionFactory() {
         try {
-        Properties properties = new Properties();
-        properties.load(getClass().getResourceAsStream("/connection.properties"));
-        Class.forName("org.mariadb.jdbc.Driver");
-        connection = DriverManager.getConnection(properties.getProperty("url"), properties.getProperty("user"),
-                properties.getProperty("password"));
- 
+            Properties properties = new Properties();
+            properties.load(getClass().getResourceAsStream("/connection.properties"));
+            ComboPooledDataSource pool = new ComboPooledDataSource();
+            Class.forName("org.mariadb.jdbc.Driver");
+            pool.setJdbcUrl(properties.getProperty("url"));
+            pool.setUser(properties.getProperty("user"));
+            pool.setPassword(properties.getProperty("password"));
+
+            ConnectionFactory.dataSource = pool;
+
         } catch (Exception e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
 
     }
-    public static Connection recuperarConexao(){
-        return connection;
+
+    public static Connection recuperarConexao() {
+        try {
+            return dataSource.getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
